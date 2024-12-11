788c89a5d28950f63f21835d7a99056f9dfbb0d5
Hyeanwoo <wkdus7h@naver.com>
2024-12-11 14:55:31 +0900

    feat: add MetroList and connect to server

--- sources/pages/Home.tsx
+++ sources/pages/Home.tsx
@@ -2,26 +2,30 @@
 
 import { useState } from 'react';
 import { MetroLineItem, MetroLineMap } from '@/components/home';
+import MetroInfo from '../components/home/MetroList';
 
 export default function Home() {
   const [lineNumber, setLineNumber] = useState(1);
 
   return (
-    <div className="flex p-5">
-      <aside className="flex flex-col gap-2">
-        <MetroLineItem lineNumber={1} setMetroLine={() => setLineNumber(1)} />
-        <MetroLineItem lineNumber={2} setMetroLine={() => setLineNumber(2)} />
-        <MetroLineItem lineNumber={3} setMetroLine={() => setLineNumber(3)} />
-        <MetroLineItem lineNumber={4} setMetroLine={() => setLineNumber(4)} />
-        <MetroLineItem lineNumber={5} setMetroLine={() => setLineNumber(5)} />
-        <MetroLineItem lineNumber={6} setMetroLine={() => setLineNumber(6)} />
-        <MetroLineItem lineNumber={7} setMetroLine={() => setLineNumber(7)} />
-        <MetroLineItem lineNumber={8} setMetroLine={() => setLineNumber(8)} />
-        <MetroLineItem lineNumber={9} setMetroLine={() => setLineNumber(9)} />
-      </aside>
-      <main>
-        <MetroLineMap lineNumber={lineNumber} />
-      </main>
+    <div className="flex p-5 flex-col gap-3">
+      <div className="flex">
+        <aside className="flex flex-col gap-2">
+          <MetroLineItem lineNumber={1} setMetroLine={() => setLineNumber(1)} />
+          <MetroLineItem lineNumber={2} setMetroLine={() => setLineNumber(2)} />
+          <MetroLineItem lineNumber={3} setMetroLine={() => setLineNumber(3)} />
+          <MetroLineItem lineNumber={4} setMetroLine={() => setLineNumber(4)} />
+          <MetroLineItem lineNumber={5} setMetroLine={() => setLineNumber(5)} />
+          <MetroLineItem lineNumber={6} setMetroLine={() => setLineNumber(6)} />
+          <MetroLineItem lineNumber={7} setMetroLine={() => setLineNumber(7)} />
+          <MetroLineItem lineNumber={8} setMetroLine={() => setLineNumber(8)} />
+          <MetroLineItem lineNumber={9} setMetroLine={() => setLineNumber(9)} />
+        </aside>
+        <main>
+          <MetroLineMap lineNumber={lineNumber} />
+        </main>
+      </div>
+      <MetroInfo />
     </div>
   );
 }
