a1f1d357ad6e34c1b7a2a020ee28a6a08041f613
Hyeanwoo <wkdus7h@naver.com>
2024-12-12 20:52:54 +0900

    feat: temporary connection

---
+++ sources/api/types/index.ts
@@ -0,0 +1,27 @@
+//  rowNum          : 데이터 인덱스 번호
+//  selectedCount   : 총 지하철 갯수
+//  subwayId        : 지하철 호선 id
+//  subwayNm        : 지하철 호선 이름
+//  statnId         : 현재역 id
+//  statnNm         : 현재역 이름
+//  trainNo         : 열차번호
+//  lastRecptnDt    : 최종 수신 일자
+//  recptnDt        : 최종 수신 시간
+//  updnLine        : 상하행 구분
+//  statnTid        : 종착역 id
+//  statnTnm        : 종착역 이름
+//  trainSttus      : 열차 상태 구분(1: 항상 도착)
+//  directAt        : 급행 여부(0: 항상 일반)
+//  lstcarAt        : 막차 여부(0: 항상 아님)
+
+// export type MetroItemType {
+
+// }
+
+// export type MetroListResponse {
+//   errorMessage: {
+//     status: 200,
+//     total: totalMetroInService,
+//   },
+//   realtimePositionList,
+// }
