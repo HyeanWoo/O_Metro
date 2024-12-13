aa7389ab2613d4fd1502486a7f4dac0492ff269c
Hyeanwoo <wkdus7h@naver.com>
2024-12-13 16:42:50 +0900

    feat: add metro response type

--- sources/api/types/index.ts
+++ sources/api/types/index.ts
@@ -1,3 +1,52 @@
+import {
+  ExpressStatus,
+  LastStatus,
+  MetroLine,
+  MetroLineNameKor,
+  StatusCode,
+  TrainStateStatus,
+  UpDownLine,
+} from '@/utils/constants';
+
+export type MetroLineKeyType = keyof typeof MetroLine;
+export type MetroLineType = (typeof MetroLine)[MetroLineKeyType];
+export type MetroLineNameKorType = (typeof MetroLineNameKor)[MetroLineKeyType];
+
+export type UpDownLineType = (typeof UpDownLine)[keyof typeof UpDownLine];
+export type TrainStateStatusType =
+  (typeof TrainStateStatus)[keyof typeof TrainStateStatus];
+export type ExpressStatusType =
+  (typeof ExpressStatus)[keyof typeof ExpressStatus];
+export type LastStatusType = (typeof LastStatus)[keyof typeof LastStatus];
+
+export type StatusCodeType = keyof typeof StatusCode;
+
+export type MetroPositionType = {
+  rowNum: number;
+  selectedCount: number;
+  subwayId: MetroLineType;
+  subwayNm: MetroLineNameKorType;
+  statnId: string;
+  statnNm: string;
+  trainNo: string;
+  lastRecptnDt: string;
+  recptnDt: string;
+  updnLine: UpDownLineType;
+  statnTid: string;
+  statnTnm: string;
+  trainSttus: TrainStateStatusType;
+  directAt: ExpressStatusType;
+  lstcarAt: LastStatusType;
+};
+export type MetroListResponse = {
+  errorMessage: {
+    status: number;
+    code: StatusCodeType;
+    total: number;
+  };
+  realtimePositionList: MetroPositionType[];
+};
+
 //  rowNum          : 데이터 인덱스 번호
 //  selectedCount   : 총 지하철 갯수
 //  subwayId        : 지하철 호선 id
@@ -13,15 +62,3 @@
 //  trainSttus      : 열차 상태 구분(1: 항상 도착)
 //  directAt        : 급행 여부(0: 항상 일반)
 //  lstcarAt        : 막차 여부(0: 항상 아님)
-
-// export type MetroItemType {
-
-// }
-
-// export type MetroListResponse {
-//   errorMessage: {
-//     status: 200,
-//     total: totalMetroInService,
-//   },
-//   realtimePositionList,
-// }
---
+++ sources/utils/constants.ts
@@ -0,0 +1,113 @@
+export const MetroLine = {
+  line1: '1001',
+  line2: '1002',
+  line3: '1003',
+  line4: '1004',
+  line5: '1005',
+  line6: '1006',
+  line7: '1007',
+  line8: '1008',
+  line9: '1009',
+} as const;
+
+export const MetroLineNameKor = {
+  line1: '1호선',
+  line2: '2호선',
+  line3: '3호선',
+  line4: '4호선',
+  line5: '5호선',
+  line6: '6호선',
+  line7: '7호선',
+  line8: '8호선',
+  line9: '9호선',
+} as const;
+
+export const StationList = [
+  { id: 1004000409, name: '당고개' },
+  { id: 1004000410, name: '상계' },
+  { id: 1004000411, name: '노원' },
+  { id: 1004000412, name: '창동' },
+  { id: 1004000413, name: '쌍문' },
+  { id: 1004000414, name: '수유' },
+  { id: 1004000415, name: '미아' },
+  { id: 1004000416, name: '미아사거리' },
+  { id: 1004000417, name: '길음' },
+  { id: 1004000418, name: '성신여대입구' },
+  { id: 1004000419, name: '한성대입구' },
+  { id: 1004000420, name: '혜화' },
+  { id: 1004000421, name: '동대문' },
+  { id: 1004000422, name: '동대문역사문화공원' },
+  { id: 1004000423, name: '충무로' },
+  { id: 1004000424, name: '명동' },
+  { id: 1004000425, name: '회현' },
+  { id: 1004000426, name: '서울' },
+  { id: 1004000427, name: '숙대입구' },
+  { id: 1004000428, name: '삼각지' },
+  { id: 1004000429, name: '신용산' },
+  { id: 1004000430, name: '이촌' },
+  { id: 1004000431, name: '동작' },
+  { id: 1004000432, name: '총신대입구(이수)' },
+  { id: 1004000433, name: '사당' },
+  { id: 1004000434, name: '남태령' },
+  { id: 1004000435, name: '선바위' },
+  { id: 1004000436, name: '경마공원' },
+  { id: 1004000437, name: '대공원' },
+  { id: 1004000438, name: '과천' },
+  { id: 1004000439, name: '정부과천청사' },
+  { id: 1004000440, name: '인덕원' },
+  { id: 1004000441, name: '평촌' },
+  { id: 1004000442, name: '범계' },
+  { id: 1004000443, name: '금정' },
+  { id: 1004000444, name: '산본' },
+  { id: 1004000445, name: '수리산' },
+  { id: 1004000446, name: '대야미' },
+  { id: 1004000447, name: '반월' },
+  { id: 1004000448, name: '상록수' },
+  { id: 1004000449, name: '한대앞' },
+  { id: 1004000450, name: '중앙' },
+  { id: 1004000451, name: '고잔' },
+  { id: 1004000452, name: '초지' },
+  { id: 1004000453, name: '안산' },
+  { id: 1004000454, name: '신길온천' },
+  { id: 1004000455, name: '정왕' },
+  { id: 1004000456, name: '오이도' },
+];
+
+export const UpDownLine = {
+  upAndClockwise: '0',
+  downAndCounterClockwise: '1',
+} as const;
+export const TrainStateStatus = {
+  entered: '0',
+  arrived: '1',
+  departed: '2',
+  preDeparted: '3',
+} as const;
+export const ExpressStatus = {
+  normal: '0',
+  express: '1',
+  limited: '7',
+} as const;
+export const LastStatus = {
+  normal: '0',
+  last: '1',
+} as const;
+
+export const StatusCode = {
+  'INFO-000': '정상 처리되었습니다.',
+  'ERROR-300': '필수 값이 누락되어 있습니다.',
+  'INFO-100': '인증키가 유효하지 않습니다.',
+  'ERROR-301': '파일타입 값이 누락 혹은 유효하지 않습니다.',
+  'ERROR-310': '해당하는 서비스를 찾을 수 없습니다.',
+  'ERROR-331': '요청시작위치 값을 확인하십시오.	',
+  'ERROR-332': '요청종료위치 값을 확인하십시오.',
+  'ERROR-333': '요청위치 값의 타입이 유효하지 않습니다.',
+  'ERROR-334': '요청종료위치 보다 요청시작위치가 더 큽니다.',
+  'ERROR-335':
+    '샘플데이터(샘플키:sample) 는 한번에 최대 5건을 넘을 수 없습니다.',
+  'ERROR-336': '데이터요청은 한번에 최대 1000건을 넘을 수 없습니다.',
+  'ERROR-500': '서버 오류입니다.',
+  'ERROR-600': '데이터베이스 연결 오류입니다.',
+  'ERROR-601': 'SQL 문장 오류 입니다.',
+  'INFO-200': '해당하는 데이터가 없습니다.',
+} as const;
