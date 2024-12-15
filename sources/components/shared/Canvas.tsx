ba1f3ff07fdf750c62d5d158f140f5f825d09d23
Hyeanwoo <wkdus7h@naver.com>
2024-12-15 22:25:57 +0900

    feat: add useCanvas hook

--- sources/components/shared/Canvas.tsx
+++ sources/components/shared/Canvas.tsx
@@ -1,3 +1,24 @@
-export default function Canvas() {
-  return <canvas width={800} height={600} />;
+import { RefObject } from 'react';
+
+interface CanvasProps {
+  canvasRef: RefObject<HTMLCanvasElement>;
+  width?: number;
+  height?: number;
+  className?: string;
+}
+
+export default function Canvas({
+  canvasRef,
+  height,
+  width,
+  className,
+}: CanvasProps) {
+  return (
+    <canvas
+      ref={canvasRef}
+      width={width}
+      height={height}
+      className={className}
+    />
+  );
 }
